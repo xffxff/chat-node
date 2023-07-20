2947f25e0770e3cdef874ffafca98dbbe716ffec
xffxff <1247714429@qq.com>
2023-07-20 09:51:02 +0000

    using source maps

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
