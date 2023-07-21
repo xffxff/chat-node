e44bbd4e874da7edcc576fdf92913424696e5768
xffxff <1247714429@qq.com>
2023-07-21 01:56:18 +0000

    make graph panel of litegraph to work

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import './node'
 import 'litegraph.js/css/litegraph.css'
 
 
-const canvasElement = document.createElement("canvas");
-canvasElement.width = window.innerWidth;
-canvasElement.height = window.innerHeight;
-document.body.appendChild(canvasElement);
+function component() {
+    const root = document.createElement("div");
+    // add 'litegraph' class to root element is required for some components of litegraph.js to work
+    root.classList.add("litegraph")
+
+    const canvasElement = document.createElement("canvas");
+    canvasElement.id = "canvas";
+    canvasElement.width = window.innerWidth;
+    canvasElement.height = window.innerHeight;
+    root.appendChild(canvasElement);
+
+    return root;
+}
+
+document.body.appendChild(component());
 
 const graph = new LGraph();
-const canvas = new LGraphCanvas(canvasElement, graph);
+const canvas = new LGraphCanvas("#canvas", graph);
 graph.start();
 
