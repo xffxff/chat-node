88704cb7af648f2cbf06143aec5e485d9d7321f5
xffxff <1247714429@qq.com>
2023-07-24 00:22:41 +0000

    add a settings modal to set openai api key

---
+++ src/editor/main.js
@@ -0,0 +1,110 @@
+import { LGraph, LGraphCanvas } from "litegraph.js/build/litegraph.core";
+import "litegraph.js/css/litegraph.css";
+import "./main.css"
+
+
+class Editor {
+
+    constructor() {
+        const html = `
+            <div class="litegraph">
+                <div class="header"></div>
+                <div class="content">
+                    <canvas id="canvas"></canvas>
+                </div>
+            </div>
+            <div></div>
+        `;
+        document.body.innerHTML = html;
+
+        this.graph = new LGraph();
+        this.canvas = new LGraphCanvas("#canvas", this.graph);
+
+        this.#resizeCanvas();
+        this.#configurateHeader();
+    }
+
+    #resizeCanvas() {
+        const canvasElement = document.querySelector("#canvas");
+        const dpr = window.devicePixelRatio || 1;
+        const { width, height } = canvasElement.parentNode.getBoundingClientRect();
+        console.log(width, height);
+        canvasElement.width = width * dpr;
+        canvasElement.height = height * dpr;
+        canvasElement.style.width = width + "px";
+        canvasElement.style.height = height + "px";
+        const ctx = canvasElement.getContext("2d");
+        ctx.scale(dpr, dpr);
+    }
+
+    #configurateHeader() {
+        const headerElement = document.querySelector(".header");
+        const html = `
+            <button class='btn execute'>Execute</button>
+            <button class='btn settings'>Settings</button>
+        `;
+        headerElement.innerHTML = html;
+
+        const executeBtn = document.querySelector(".btn.execute");
+        executeBtn.addEventListener("click", () => {
+            for (const id in this.canvas.selected_nodes) {
+                const node = this.canvas.selected_nodes[id];
+                node.doExecute();
+            }
+        });
+
+        this.#settingsModal();
+    }
+
+    #settingsModal() {
+        const html = `
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <form class="form">
+                    <div class="api-key">
+                        <label for="api-key">OpenAI API Key</label>
+                        <input type="text" id="api-key" name="api-key" placeholder="Enter your API key">
+                    </div>
+                    <div class="save">
+                        <input type="submit" value="Save">
+                    </div>
+                </form>
+            </div>
+        `;
+        const modal = document.createElement("div");
+        modal.classList.add("settings-modal");
+        modal.innerHTML = html;
+        document.body.appendChild(modal);
+
+        // when the user clicks the settings button, open the modal
+        const settingsBtn = document.querySelector(".btn.settings");
+        settingsBtn.addEventListener("click", () => {
+            modal.style.display = "block";
+        });
+
+        // when the user clicks on the close button, close the modal
+        const closeSpan = document.querySelector(".close");
+        closeSpan.addEventListener("click", () => {
+            modal.style.display = "none";
+        });
+
+        // when the user clicks anywhere outside of the modal, close it
+        window.addEventListener("click", (event) => {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        });
+
+        // when the user clicks the save button, save the api key and close the modal
+        const form = document.querySelector(".form");
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            const apiKey = document.querySelector("#api-key").value;
+            // FIXME: save the api key
+            console.log(apiKey);
+            modal.style.display = "none";
+        });
+    }
+}
+
+export default Editor;
--- src/index.js
+++ src/index.js
@@ -1,64 +1,12 @@
-import {
-    LGraph,
-    LGraphCanvas,
-    LiteGraph,
-} from "litegraph.js/build/litegraph.core.js";
+import Editor from "./editor/main";
+import "./style.css"
 import "./node";
-import "litegraph.js/css/litegraph.css";
-import "./style.css";
+import { LiteGraph } from "litegraph.js/build/litegraph.core";
 
-function createCanvas(parent) {
-    const canvas = document.createElement("canvas");
-    canvas.id = "canvas";
+const editor = new Editor();
 
-    // the following code is for high resolution displays
-    const dpr = window.devicePixelRatio || 1;
-    canvas.width = window.innerWidth * dpr;
-    canvas.height = window.innerHeight * dpr;
-    canvas.style.width = window.innerWidth + "px";
-    canvas.style.height = window.innerHeight + "px";
-    const ctx = canvas.getContext("2d");
-    ctx.scale(dpr, dpr);
-
-    parent.appendChild(canvas);
-}
-
-function createHeader(parent) {
-    const html = `
-        <div class="header">
-            <button class='btn execute'>Execute</button>
-            <button class='btn settings'>Settings</button>
-        </div>
-    `
-    parent.innerHTML = html;
-}
-
-const root = document.createElement("div");
-root.classList.add("litegraph");
-document.body.appendChild(root);
-
-createHeader(root);
-createCanvas(root);
-
-const graph = new LGraph();
-const canvas = new LGraphCanvas("#canvas", graph);
 const node = LiteGraph.createNode("widget/text");
 node.pos = [600, 500];
 node.properties.value = "Hello World!";
-graph.add(node);
-graph.start();
-
-// add a event listener to the execute button
-const btn = document.querySelector(".btn.execute");
-btn.addEventListener("click", () => {
-    for (const id in canvas.selected_nodes) {
-        const node = canvas.selected_nodes[id];
-        node.doExecute();
-    }
-})
-
-// add a event listener to the settings button
-const settingsBtn = document.querySelector(".btn.settings");
-settingsBtn.addEventListener("click", () => {
-    console.log("settingsBtn clicked");
-});
+editor.graph.add(node);
+editor.graph.start();
